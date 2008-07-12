c8571b8e8685a194f084caf57c083b80d50bfe82
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2008-07-12 04:56:04 +0000

    JAXB 2.0 examples added

---
+++ JAXB/classgen.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+# Compartible with Java SE 1.6 
+
+$JAVA_HOME/bin/xjc -d src -p org.localstorm.jaxb.bound schema2.xsd  
---
+++ JAXB/schemagen.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+# Compartible with Java SE 1.6 
+
+$JAVA_HOME/bin/schemagen -d schema src/org/localstorm/jaxb/bound/Shiporder.java
