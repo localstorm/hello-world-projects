81f8c9bf979d64529049b7ba9c0ea1e660de3788
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2008-07-16 04:59:49 +0000

    JUnitEE initial added

---
+++ trunk/JUnitEETest/src/ru/mss/ip/pms/techlog/tests/CmdLoggerServiceTest.java
@@ -0,0 +1,26 @@
+package ru.mss.ip.pms.techlog.tests;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: akuznetsov
+ * Date: 22.05.2007
+ * Time: 16:29:14
+ */
+public class CmdLoggerServiceTest extends TestCase
+{
+
+    public CmdLoggerServiceTest(String name)
+    {
+        super(name);
+    }
+
+    public void test() throws Exception 
+    {
+    }
+
+    protected void setUp() throws Exception {
+    }
+
+}
