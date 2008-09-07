fe865e774f23613340d83ec6678ddd32e032f0bb
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2008-09-07 06:20:12 +0000

    Reference obj creation added

--- GTDMCC-Project/src/org/localstorm/mcc/ejb/referenced/ReferencedObject.java
+++ GTDMCC-Project/src/org/localstorm/mcc/ejb/referenced/ReferencedObject.java
@@ -4,12 +4,17 @@ import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import org.localstorm.mcc.ejb.Identifiable;
+import org.localstorm.mcc.ejb.contexts.Context;
 
 
 /**
@@ -17,7 +22,7 @@ import javax.persistence.TemporalType;
  */
 @Entity
 @Table(name="REFERENCED_OBJECTS")
-public class ReferencedObject implements Serializable 
+public class ReferencedObject implements Serializable, Identifiable 
 {   
     
     @Id
@@ -34,22 +39,35 @@ public class ReferencedObject implements Serializable
     @Temporal(TemporalType.TIMESTAMP)
     private Date creation;
     
+    @JoinColumn(name="context_id", nullable=false)
+    @ManyToOne(fetch=FetchType.EAGER)
+    private Context context;
+    
+    @Column(name="sort_order", unique=false, updatable=true, nullable=false )    
+    private Integer sortOrder;
+
+    
     public ReferencedObject() 
     {
     
     }
 
-    public ReferencedObject( String name ) {
-        this.name = name;
-        this.archived = false;
-        this.creation = new Date();
+    public ReferencedObject( String name, Context ctx ) {
+        this.name      = name;
+        this.archived  = false;
+        this.creation  = new Date();
+        this.context   = ctx;
+        this.sortOrder = Integer.valueOf(1);
     }
-
     
     public Integer getId() {
         return id;
     }
 
+    public Context getContext() {
+        return context;
+    }
+    
     public Date getCreation() {
         return creation;
     }
@@ -73,4 +91,9 @@ public class ReferencedObject implements Serializable
     public void setCreation(Date creation) {
         this.creation = creation;
     }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+    
 }
