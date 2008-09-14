2c4862df48e235f2c9c6f33976d58423a205c4d7
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2008-09-14 05:17:36 +0000

    password change feature added

--- GTDMCC-Project/src/org/localstorm/mcc/web/actions/ConfigUpdateActionBean.java
+++ GTDMCC-Project/src/org/localstorm/mcc/web/actions/ConfigUpdateActionBean.java
@@ -4,6 +4,13 @@ import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.validation.SimpleError;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidationError;
+import net.sourceforge.stripes.validation.ValidationErrors;
+import net.sourceforge.stripes.validation.ValidationMethod;
+import net.sourceforge.stripes.validation.ValidationState;
+import org.localstorm.mcc.ejb.users.UserManager;
 import org.localstorm.mcc.web.Views;
 
 
@@ -16,10 +23,29 @@ import org.localstorm.mcc.web.Views;
 @UrlBinding("/actions/UpdateConfig")
 public class ConfigUpdateActionBean extends ContextsEditActionBean {
 
+    @Validate(required=true)
     private String oldPassword;
+    
+    @Validate(minlength=5, required=true)
     private String password;
+    
+    @Validate(minlength=5, required=true)
     private String password2;
 
+    @ValidationMethod(when=ValidationState.NO_ERRORS) 
+    public void validatePassword(ValidationErrors errors) { 
+        UserManager um = this.getUserManager();
+        if (um.login(this.getUser().getLogin(), oldPassword)==null)
+        {
+            errors.add("oldPassword", new SimpleError("Old passwrod is not valid."));
+        }
+        if (!password.equals(password2))
+        {
+            errors.add("password", new SimpleError("New password values are not the same."));
+        }
+    }
+
+    
     public void setPassword(String password) {
         this.password = password;
     }
@@ -34,6 +60,8 @@ public class ConfigUpdateActionBean extends ContextsEditActionBean {
     
     @DefaultHandler
     public Resolution configure() {
+        UserManager um = this.getUserManager();
+        um.changePassword(this.getUser(), this.password);
         return new ForwardResolution( Views.CONFIGURE );
     }
     
