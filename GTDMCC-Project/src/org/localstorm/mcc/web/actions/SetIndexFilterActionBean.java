907051bf794360ff7a74139813a4d9d484535ec4
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2008-11-16 05:09:52 +0000

    some generalization

--- GTDMCC-Project/src/org/localstorm/mcc/web/actions/SetIndexFilterActionBean.java
+++ GTDMCC-Project/src/org/localstorm/mcc/web/actions/SetIndexFilterActionBean.java
@@ -5,10 +5,6 @@ import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.RedirectResolution;
 import net.sourceforge.stripes.action.UrlBinding;
 import net.sourceforge.stripes.validation.Validate;
-import org.localstorm.mcc.ejb.contexts.ContextManager;
-import org.localstorm.mcc.ejb.except.ObjectNotFoundException;
-import org.localstorm.mcc.web.SessionKeys;
-import org.localstorm.mcc.web.util.SessionUtil;
 
 /**
  * A very simple calculator action.
@@ -28,20 +24,9 @@ public class SetIndexFilterActionBean extends BaseActionBean {
     }
     
     @DefaultHandler
-    public Resolution filling(){
-        
-        if (this.getContextId()!=-1) {
-            ContextManager cm = super.getContextManager();
-            try
-            {
-                cm.findById(this.getContextId());    
-            }catch(ObjectNotFoundException e){
-                this.setContextId(-1);
-            }
-        }
-        
-        SessionUtil.fill(this.getSession(), SessionKeys.FILTER_CONTEXT, this.getContextId());
-        return new RedirectResolution(IndexActionBean.class);
+    public Resolution filling() {
+       super.setContextIdFilter(contextId);
+       return new RedirectResolution(IndexActionBean.class);
     }
 
 }
