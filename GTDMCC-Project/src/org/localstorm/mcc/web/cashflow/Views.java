6dba208a96dfcd6a91b94e9b67e02b42b3a3ee5f
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-01-10 04:31:27 +0000

    Balance History chart added

--- GTDMCC-Project/src/org/localstorm/mcc/web/cashflow/Views.java
+++ GTDMCC-Project/src/org/localstorm/mcc/web/cashflow/Views.java
@@ -12,6 +12,7 @@ public class Views
     public static final String VIEW_TARGET          = "/jsp/cashflow/viewTarget.jsp";
     public static final String VIEW_ASSET           = "/jsp/cashflow/viewAsset.jsp";
     public static final String ASSETS_STRUCTURE     = "/jsp/cashflow/assetsStructure.jsp";
+    public static final String BALANCE_HISTORY      = "/jsp/cashflow/balanceHistory.jsp";
     public static final String WEALTH_HISTORY       = "/jsp/cashflow/netWealthHistory.jsp";
     public static final String OPS_LOG              = "/jsp/cashflow/operationsLog.jsp";
     public static final String VIEW_ASSET_COST_HISTORY = "/jsp/cashflow/assetCosts.jsp";
---
+++ GTDMCC-Project/src/org/localstorm/mcc/web/cashflow/charting/BalanceHistoryChartGenerator.java
@@ -0,0 +1,120 @@
+package org.localstorm.mcc.web.cashflow.charting;
+
+import java.awt.Color;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.localstorm.mcc.ejb.ContextLookup;
+import org.localstorm.mcc.ejb.cashflow.stat.HistoricalValue;
+import org.localstorm.mcc.ejb.cashflow.stat.HistoricalValuesManager;
+import org.localstorm.mcc.ejb.cashflow.stat.ValueType;
+import org.localstorm.mcc.ejb.users.User;
+import org.localstorm.mcc.web.Constants;
+
+/**
+ *
+ * @author localstorm
+ */
+public class BalanceHistoryChartGenerator {
+
+     private static XYDataset getBalanceHistoryDataset(User user, Integer daysPeriod) {
+
+        Calendar cal = Calendar.getInstance();
+
+        if ( daysPeriod==null ) {
+            cal.add(Calendar.YEAR, -50); // 50 years
+        } else {
+            cal.add(Calendar.DATE, -daysPeriod);
+        }
+
+        HistoricalValuesManager hvm = ContextLookup.lookup(HistoricalValuesManager.class,
+                                                           HistoricalValuesManager.BEAN_NAME);
+        
+        Collection<HistoricalValue> hvs = hvm.findByValueTag(ValueType.BALANCE_CHECKPOINT,
+                                                             user,
+                                                             cal.getTime());
+
+        HistoricalValue last  = hvm.findLastByValueTag(ValueType.BALANCE_CHECKPOINT,
+                                                       BigDecimal.ZERO,
+                                                       user);
+
+        Date minDate = new Date();
+
+        if (hvs.isEmpty()) {
+            HistoricalValue first = new HistoricalValue();
+            {
+                first.setFixDate(cal.getTime());
+                first.setObjectId(null);
+                first.setOwner(user);
+                first.setVal(last.getVal());
+                first.setValueTag(ValueType.BALANCE_CHECKPOINT);
+            }
+            hvs.add(first);
+        }
+
+        HistoricalValue right = new HistoricalValue();
+        {
+            right.setFixDate(new Date());
+            right.setObjectId(null);
+            right.setOwner(user);
+            right.setVal(last.getVal());
+            right.setValueTag(ValueType.BALANCE_CHECKPOINT);
+        }
+
+        hvs.add(right);
+  
+        TimeSeries balance     = new TimeSeries("Balance");
+
+        TimeSeriesCollection tsc = new TimeSeriesCollection();
+
+        for (HistoricalValue hv : hvs) {
+            Date fixDate = hv.getFixDate();
+            balance.addOrUpdate(new Day(fixDate), hv.getVal());
+
+            if (minDate.after(fixDate))
+            {
+                minDate = fixDate;
+            }
+        }
+
+        tsc.addSeries(balance);
+
+        return tsc;
+    }
+
+    public static JFreeChart getChart(User user, Integer daysOffset, String name) {
+        XYDataset dataset = getBalanceHistoryDataset(user, daysOffset);
+
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(name,
+                                                              "Time line",
+                                                              "Money",
+                                                              dataset,
+                                                              true,
+                                                              true,
+                                                              false);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        {
+            DateAxis axis = (DateAxis) plot.getDomainAxis();
+            axis.setDateFormatOverride(new SimpleDateFormat(Constants.REDUCED_DATE_FORMAT));
+            plot.setForegroundAlpha(0.7f);
+            plot.setNoDataMessage("No data to display");
+            plot.setRangeGridlinePaint(Color.WHITE);
+            plot.setDomainGridlinePaint(Color.WHITE);
+            plot.setBackgroundPaint(Color.LIGHT_GRAY);
+        }
+
+        return chart;
+    }
+
+}
