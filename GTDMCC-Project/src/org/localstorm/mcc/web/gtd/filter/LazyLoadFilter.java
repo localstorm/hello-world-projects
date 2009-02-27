7153a7ddb8f4523cb8e55fe03d21e05f9831a0ad
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-02-27 03:57:36 +0000

    Archived contexts are added to security context

--- GTDMCC-Project/src/org/localstorm/mcc/web/gtd/filter/LazyLoadFilter.java
+++ GTDMCC-Project/src/org/localstorm/mcc/web/gtd/filter/LazyLoadFilter.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +16,6 @@ import javax.servlet.http.HttpSession;
 import org.localstorm.mcc.ejb.ContextLookup;
 import org.localstorm.mcc.ejb.gtd.contexts.Context;
 import org.localstorm.mcc.ejb.gtd.contexts.ContextManager;
-import org.localstorm.mcc.ejb.gtd.tasks.TaskManager;
 import org.localstorm.mcc.ejb.users.User;
 import org.localstorm.mcc.web.SessionKeys;
 import org.localstorm.mcc.web.util.SessionUtil;
@@ -51,6 +49,20 @@ public class LazyLoadFilter implements Filter
         
     }
 
+    private void fillAccessibleContexts(List<Context> ctxs, Collection<Context> actxList, HttpSession sess)
+    {
+        Map<Integer, Boolean> acm = new HashMap<Integer, Boolean>();
+        for (Context ctx:ctxs)
+        {
+            acm.put(ctx.getId(), Boolean.TRUE);
+        }
+        for (Context ctx:actxList)
+        {
+            acm.put(ctx.getId(), Boolean.TRUE);
+        }
+        SessionUtil.fill(sess, SessionKeys.ACCESSIBLE_CONTEXTS_MAP, acm);
+    }
+
     private void performLazyLoad(HttpServletRequest req, HttpServletResponse res) {
         HttpSession sess = req.getSession(true);
         User user = (User) sess.getAttribute(SessionKeys.USER);
@@ -62,24 +74,17 @@ public class LazyLoadFilter implements Filter
         if ( SessionUtil.isEmpty(sess, SessionKeys.CONTEXTS) ) {
             ContextManager cm = ContextLookup.lookup(ContextManager.class, 
                                                      ContextManager.BEAN_NAME);
-            Collection<Context> ctxList = cm.findByOwner(user);
+            
+            Collection<Context> ctxList = cm.find(user);
+            Collection<Context> actxList = cm.findArchived(user);
+
             List<Context> ctxs = new ArrayList<Context>(ctxList);
             
-            Collections.sort(ctxs, new Comparator<Context>() {
-                @Override
-                public int compare(Context o1, Context o2) {
-                    return o1.getName().compareTo(o2.getName());
-                }
-            });
+            Collections.sort(ctxs, new Context.NameAscComparator());
             
             SessionUtil.fill(sess, SessionKeys.CONTEXTS, ctxs);
 
-            Map<Integer, Boolean> acm = new HashMap<Integer, Boolean>();
-            for (Context ctx : ctxs) {
-                acm.put(ctx.getId(), Boolean.TRUE);
-            }
-
-            SessionUtil.fill(sess, SessionKeys.ACCESSIBLE_CONTEXTS_MAP, acm);
+            this.fillAccessibleContexts(ctxs, actxList,sess);
         }
         
         if ( SessionUtil.isEmpty(sess, SessionKeys.REFERENCE_OBJECTS) ) {
