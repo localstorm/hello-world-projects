91c8753710c78a008fbb86727f30742a6c823840
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-04-18 10:24:47 +0000

    BeanShell hello world

---
+++ trunk/BeanShellTest/src/org/localstorm/bsh/Main.java
@@ -0,0 +1,18 @@
+package org.localstorm.bsh;
+
+import bsh.Console;
+
+/**
+ *
+ * @author localstorm
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Console.main(new String[0]);
+    }
+
+}
---
+++ trunk/BeanShellTest/src/org/localstorm/bsh/MegaLocator.java
@@ -0,0 +1,18 @@
+package org.localstorm.bsh;
+
+/**
+ *
+ * @author Alexey Kuznetsov
+ */
+public class MegaLocator 
+{
+
+    public MegaLocator(String host, int port)
+    {
+    }
+
+    public String locate()
+    {
+        return "MyService";
+    }
+}
