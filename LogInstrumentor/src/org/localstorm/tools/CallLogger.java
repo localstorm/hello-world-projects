1c05a75b01fe9e6165f468447a215c5207dc0bcf
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-04-19 11:04:28 +0000

    LogInstrumentor improved

--- LogInstrumentor/src/org/localstorm/tools/CallLogger.java
+++ LogInstrumentor/src/org/localstorm/tools/CallLogger.java
@@ -1,10 +1,21 @@
 package org.localstorm.tools;
 
+
 /**
- *
  * @author Alexey Kuznetsov
  */
-public class CallLogger 
+public class CallLogger
 {
+    private static final CallLogger ctl = new CallLogger();
 
+    public static CallLogger getInstance()
+    {
+        return ctl;
+    }
+            
+    public synchronized void logTime(String className, String methodName, long actualTime)
+    {
+        //throw new RuntimeException("Ops");
+        System.out.println("Timing: "+className+"."+methodName+" ("+actualTime+" ms.)");
+    }
 }
