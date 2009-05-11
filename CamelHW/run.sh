f72d23cd614f295f433e333075e92c728257ebb7
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-05-11 02:39:11 +0000

    Logging conf

--- CamelHW/run.sh
+++ CamelHW/run.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-#Testing with 15Mb heap
-java -Xmx48m -Dorg.apache.camel.jmx.disabled=True  -cp 'lib/*:dist/*' -Dcom.sun.management.jmxremote org.localstorm.stocktracker.Main $@
+# JConsole connections enabled
+java -Xmx48m \
+ -Dorg.apache.camel.jmx.disabled=True  \
+ -Djava.util.logging.config.file=config/logging.properties \
+ -Dcom.sun.management.jmxremote \
+ -cp 'lib/*:dist/*' \
+ org.localstorm.stocktracker.Main $@
+
+
 
 # For NetBeans Profiler
 #java -Xmx15m -agentpath:/home/localstorm/Program_Files/netbeans-6.5/profiler3/lib/deployed/jdk15/linux/libprofilerinterface.so=/home/localstorm/Program_Files/netbeans-6.5/profiler3/lib,5140  -cp 'lib/*:dist/*' -Dcom.sun.management.jmxremote org.localstorm.stocktracker.Main 
