bf29fb35cc0eb91d86eae0008da8c2ec9c9007af
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-05-11 09:15:22 +0000

    Finally done

--- CamelHW/run.sh
+++ CamelHW/run.sh
@@ -8,7 +8,5 @@ java \
  -cp 'lib/*:dist/*' \
  org.localstorm.stocktracker.Main $@
 
-
-
 # For NetBeans Profiler
-java #-agentpath:/home/localstorm/Program_Files/netbeans-6.5/profiler3/lib/deployed/jdk15/linux/libprofilerinterface.so=/home/localstorm/Program_Files/netbeans-6.5/profiler3/lib,5140  -cp 'lib/*:dist/*' -Dcom.sun.management.jmxremote org.localstorm.stocktracker.Main 
+#java -agentpath:/home/localstorm/Program_Files/netbeans-6.5/profiler3/lib/deployed/jdk15/linux/libprofilerinterface.so=/home/localstorm/Program_Files/netbeans-6.5/profiler3/lib,5140  -cp 'lib/*:dist/*' -Dcom.sun.management.jmxremote org.localstorm.stocktracker.Main 
