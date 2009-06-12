619f2ab37f2e021a078715d82ded759bc03faac5
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-06-12 03:18:20 +0000

    Days before birthday sql query fixed

--- trunk/GTDMCC-Project/ear-res/META-INF/people-dashboard-report.sql
+++ trunk/GTDMCC-Project/ear-res/META-INF/people-dashboard-report.sql
@@ -1,3 +1,4 @@
-select remains, id, name, lname, pname, birth_date from 
-    (select DAYOFYEAR(birth_date)-DAYOFYEAR(CURRENT_DATE) remains, id, name, lname, pname, birth_date  from PERSONS where birth_date is not null ORDER BY remains ASC) a
-where remains>=0 and id IN (select person_id from PERSONS_TO_GROUPS where group_id IN (select id from PERSON_GROUPS where owner=?) )
+select remains, id, name, lname, pname, birth_date from
+    (SELECT DAYOFYEAR(birth_date + INTERVAL YEAR(NOW()) - YEAR(birth_date) YEAR) - DAYOFYEAR(NOW()) remains, id, name, lname, pname, birth_date FROM PERSONS
+    where birth_date is not null ORDER BY remains ASC) a
+where remains>=0 and id IN (select person_id from PERSONS_TO_GROUPS where group_id IN (select id from PERSON_GROUPS where owner=?))
