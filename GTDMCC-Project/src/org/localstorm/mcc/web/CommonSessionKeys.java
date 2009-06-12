6fccbe81710eb9980ac7584a06feab4164970b3f
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-06-12 09:02:13 +0000

    Correct URL resubmission added (after sign-on)

--- GTDMCC-Project/src/org/localstorm/mcc/web/CommonSessionKeys.java
+++ GTDMCC-Project/src/org/localstorm/mcc/web/CommonSessionKeys.java
@@ -8,4 +8,5 @@ public interface CommonSessionKeys
 {
     public static final String USER                   = "user";
     public static final String RETURN_PAGE_BEAN_REFIX = "rpb-";
+    public static final String ORIGINAL_REQUEST       = "$originalRequest$";
 }
