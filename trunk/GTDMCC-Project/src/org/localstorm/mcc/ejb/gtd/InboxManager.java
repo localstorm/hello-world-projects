cf1ad2997fc2a1b988fd5fb330f78a51f2af73c7
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-06-27 06:16:26 +0000

    Inbox pane & Inbox backend support added

---
+++ trunk/GTDMCC-Project/src/org/localstorm/mcc/ejb/gtd/InboxManager.java
@@ -0,0 +1,19 @@
+package org.localstorm.mcc.ejb.gtd;
+
+import java.util.List;
+import org.localstorm.mcc.ejb.gtd.entity.InboxEntry;
+import org.localstorm.mcc.ejb.users.User;
+
+/**
+ *
+ * @author Alexey Kuznetsov
+ */
+public interface InboxManager 
+{
+    public static final String BEAN_NAME = "InboxManagerBean";
+
+    public void submitNote(InboxEntry ie);
+    public void removeNote(InboxEntry ie);
+
+    public List<InboxEntry> getInboxEntries(User owner);
+}
