2d862d971d0583eea9fa3fea5efab0baf9124e64
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2009-10-17 14:04:01 +0000

    Some minor fixes

--- GroovyDSL/models/demo/MyAnotherComposite.groovy
+++ GroovyDSL/models/demo/MyAnotherComposite.groovy
@@ -3,12 +3,12 @@ package demo
 import org.localstorm.platform.dsl.fbp.*
 
 cool = "Cool".component(
-    filename: 'file.csv',
+    fileName: 'file.csv',
     pollForUpdates: 1000
 )
 
 uncool = "Uncool".component(
-    filename : (args.length>0) ? args[0] : null
+    compositeName : (args.length>0) ? args[0] : null
 )
 
 uncoolOutput = uncool.port("OUTPUT")
