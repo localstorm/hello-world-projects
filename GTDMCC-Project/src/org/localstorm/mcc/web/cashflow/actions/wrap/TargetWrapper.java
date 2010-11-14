3325d2b0a9431cffaad9468c67fedf6270c0f809
localstorm <localstorm@80998939-e44e-0410-96dd-c3ce0be779da>
2010-11-14 07:51:54 +0000

    Targets sorting by cost added

--- GTDMCC-Project/src/org/localstorm/mcc/web/cashflow/actions/wrap/TargetWrapper.java
+++ GTDMCC-Project/src/org/localstorm/mcc/web/cashflow/actions/wrap/TargetWrapper.java
@@ -4,8 +4,9 @@ import org.localstorm.mcc.ejb.cashflow.entity.Cost;
 import org.localstorm.mcc.ejb.cashflow.entity.Target;
 import org.localstorm.mcc.ejb.cashflow.entity.ValuableObject;
 
+import java.util.Comparator;
+
 /**
- *
  * @author localstorm
  */
 public class TargetWrapper extends Target {
@@ -15,14 +16,13 @@ public class TargetWrapper extends Target {
     private static final long serialVersionUID = 2612970717794392383L;
 
     public TargetWrapper(Target target,
-                         Cost cost)
-    {
-        if (cost==null) {
+                         Cost cost) {
+        if (cost == null) {
             throw new NullPointerException("Cost is null!");
         }
-        
+
         this.target = target;
-        this.cost   = cost;
+        this.cost = cost;
     }
 
     @Override
@@ -63,5 +63,15 @@ public class TargetWrapper extends Target {
     public Cost getCurrentCost() {
         return this.cost;
     }
-   
+
+    public static Comparator<Target> getCurrentCostComparator() {
+        return new Comparator<Target>() {
+            @Override
+            public int compare(Target o1, Target o2) {
+                TargetWrapper tw1 = (TargetWrapper) o1;
+                TargetWrapper tw2 = (TargetWrapper) o2;
+                return tw1.getCurrentCost().getBuy().compareTo(tw2.getCurrentCost().getBuy());
+            }
+        };
+    }
 }
